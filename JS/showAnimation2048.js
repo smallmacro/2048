a688943e44b5ab9d8496e2bfa4492a2338611428
macropan <smallmacro@gmail.com>
2014-06-24 16:55:11 +0800

    update

---
+++ JS/showAnimation2048.js
@@ -0,0 +1,26 @@
+function showAnimation( x, y, number ){
+	var numberCeil =  $('#number-ceil-' + x + '-' + y);
+	numberCeil.css('background-color', getNumberBackgroundColor( number ));
+	numberCeil.css('color', getNumberColor( number ));
+	numberCeil.text( number );
+
+
+	numberCeil.animate({
+		width: "100px",
+		height: "100px",
+		top: getPosTop(x, y),
+		left: getPosLeft(x, y)
+		},50);
+}
+
+function showMoveAnimation(fromx, fromy, tox, toy){
+	var numberCeil =  $('#number-ceil-' + fromx + '-' + fromy);
+	numberCeil.animate({
+		top: getPosTop(tox, toy),
+		left: getPosLeft(tox, toy)
+		},200);
+}
+
+function updateScore(score) {
+	$("#score").text(score);
+}
